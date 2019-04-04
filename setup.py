bc415884163f892c16334ab734221709a9379d26
Edmund Martin <edmartin101@googlemail.com>
2019-04-04 10:10:38 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,10 @@ from setuptools import setup
 
 base_path = os.path.dirname(__file__)
 
-with open(os.path.join(base_path, 'cfscrape', '__init__.py')) as fp:
-    VERSION = re.compile(r'.*__version__ = "(.*?)"',
-                         re.S).match(fp.read()).group(1)
-
 setup(
-  name = 'cloudscrape'
+  name = 'cloudscrape',
   packages = ['cfscrape'],
-  version = VERSION,
+  version = 0.2,
   description = 'A fork of cfscrape Cloudflare\'s anti-bot page.',
   author = 'Anorov',
   author_email = 'edmartin101@gmail.com',
